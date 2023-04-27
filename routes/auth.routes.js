ef95c4c3514e20d3fbd4a14f1efe2245647606f0
Federico <federicofarina@hotmail.com>
2023-04-27 19:04:49 +0200

    login signup

---
+++ routes/auth.routes.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const Employee = require('../models/employee.model')
+const { response } = require('../app')
+const router = express.Router()
+
+
+
+// Get to client sigup form
+
+router.get('/signup', (req, res, next) => {
+    res.render('./auth/signup')
+  })
+
+// Post values given by client in the signup form 
+
+
+
+// Get to client login form
+
+router.get('/login', (req, res, next) => {
+    res.render('./auth/login')
+
+})
+
+// Post for working with the values inside the DB for login
+
+
+
+
+module.exports = router
+
+
+
+
+
+
+
+
+module.exports = router
