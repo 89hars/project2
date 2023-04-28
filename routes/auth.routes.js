0624e99ef399b100fd56f2ec30945a4f19064e99
Hector Riveros <89.hars@gmail.com>
2023-04-28 13:04:58 +0200

    set up minimal password for login done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const bcryptjs = require('bcryptjs')
 const saltRounds = 10
 
+const pwdRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/
 
 
 // Get to client sigup form
@@ -17,22 +18,27 @@ router.get('/signup', (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   
-  const salt = bcryptjs.genSaltSync(saltRounds)
-  const passwordHash = bcryptjs.hashSync(req.body.password, salt)
   try {
  const newSignup = await Employee.findOne({username: req.body.username})
  console.log(newSignup)
 
   if(!newSignup) {
-    const newEmployee = await Employee.create({username: req.body.username, passwordHash: passwordHash})
-    console.log("New employee", newEmployee)
-    res.redirect('/auth/login')
+    if(pwdRegex.test(req.body.password)) {
+      const salt = bcryptjs.genSaltSync(saltRounds)
+      const passwordHash = bcryptjs.hashSync(req.body.password, salt)
+      
+      await Employee.create({username: req.body.username, passwordHash: passwordHash})
+      console.log("New employee", newEmployee)
+      res.redirect('/auth/login')
+
+    } else {
+      res.render('auth/signup', {errorMessage: "Password should be stronger!", data: {username: req.body.username}})
+    }
   } else {
 
     res.render('auth/signup', {errorMessage: "User name already exist"})
   }
 
-    
   } catch (error) {
     console.log(error)
   }
