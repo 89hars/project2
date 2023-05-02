5ef06d1ee67587d2862d6d8d53ed8f1ad2d65023
Federico <federicofarina@hotmail.com>
2023-05-02 21:32:15 +0200

    date & time added to checkin and out

--- routes/check.routes.js
+++ routes/check.routes.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const router = express.Router()
-const Event = require('../models/event.model')
+const Event = require('../models/event.model');
+const Employee = require('../models/employee.model');
 
 router.post('/checkin', async (req,res) => {
     console.log(req.body, "checkin")
     try {
+      //Fetch the Employee document for the logged-in user
+      const employee = await Employee.findById(req.session.user._id);
       // Create a new event document with the eventName set to "checkin"
       const checkin = new Event({eventName: 'checkin', owner: req.session.user._id });
       //Save the event to the database
       await checkin.save();
       // Send a success message
-      res.redirect("/comments")
+      const createdAtString = checkin.createdAt.toLocaleString();
+      const successMessage = `${employee.username} has checked in at ${createdAtString} `;
+      res.render('comments', { user: req.session.user.username, successMessage: successMessage });
+      //res.redirect("/comments")
     } catch (error) {
       console.error(error);
       res.status(500).send("Internal server error");
@@ -20,12 +26,17 @@ router.post('/checkin', async (req,res) => {
   router.post('/checkout', async (req,res) => {
     console.log(req.body, "checkout")
     try {
+      //Fetch the Employee document for the logged-in user
+      const employee = await Employee.findById(req.session.user._id);
       // Create a new event document with the eventName set to "checkin"
       const checkout = new Event({eventName: 'checkout', owner: req.session.user._id });
       //Save the event to the database
       await checkout.save();
       // Send a success message
-      res.redirect("/comments")
+      const createdAtString = checkout.createdAt.toLocaleString();
+      const successMessage = `${employee.username} has checked out ${createdAtString} `;
+      res.render('comments', { user: req.session.user.username, successMessage: successMessage });
+      //res.redirect("/comments")
     } catch (error) {
       console.error(error);
       res.status(500).send("Internal server error");
@@ -33,3 +44,4 @@ router.post('/checkin', async (req,res) => {
   });
 
 module.exports = router
+
