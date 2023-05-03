032ba5f656eaa5dae0e8932225705de8bf5ea8cf
Federico <federicofarina@hotmail.com>
2023-05-03 14:24:46 +0200

    solved routes bugs

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -11,7 +11,7 @@ const pwdRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/
 // Get to client sigup form
 
 router.get('/signup', isLoggedOut, (req, res, next) => {
-    res.render('./auth/signup')
+    res.render('auth/signup')
   })
 
 // Post values given by client in the signup form 
@@ -29,7 +29,7 @@ router.post('/signup', isLoggedOut, async (req, res, next) => {
       
       await Employee.create({username: req.body.username, passwordHash: passwordHash})
       console.log("New employee", newSignup)
-      res.redirect('./login')
+      res.redirect('/login')
 
     } else {
       res.render('auth/signup', {errorMessage: "Password should be stronger!", data: {username: req.body.username}})
@@ -46,7 +46,7 @@ router.post('/signup', isLoggedOut, async (req, res, next) => {
 // Get to client login form
 
 router.get('/login', isLoggedOut, (req, res, next) => {
-    res.render('./auth/login')
+    res.render('auth/login')
 })
 
 // Post for working with the values inside the DB for login
