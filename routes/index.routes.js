77fca25033cb124f53eed045197339a23880f518
Hector Riveros <89.hars@gmail.com>
2023-05-04 14:29:38 +0200

    comments all arround, new route for logout

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,17 +3,31 @@ const router = express.Router();
 const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard')
 const Comment = require('../models/comment.model')
 
-/* GET home page */
+// Handling of other routes.
+
+
+// GET home page 
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-/* Get Profile */
+// Get to send profile to user only if login is true.
 
 router.get('/profile', isLoggedIn, (req, res, next) => {
   console.log(req.session)
   res.render('profile', { user: req.session.user })
 });
+
+
+ // Log Out linked to a button in differents views.
+ router.get('/logout', isLoggedIn, (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(err);
+    res.clearCookie('connect.sid'); 
+    res.redirect('/'); 
+  });
+});
+
 
 
 module.exports = router;
